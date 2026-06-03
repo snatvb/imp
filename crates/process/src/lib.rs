1dadb0cf51c8b03fb268f26691158796c90f6100
Andrew Avsenin <snatvb@ya.ru>
2026-06-03 03:48:11 +0300

    chore: more convenient way to bind cwd

--- crates/process/src/lib.rs
+++ crates/process/src/lib.rs
@@ -1,13 +1,15 @@
 use js_core::error::SystemError;
 use rquickjs as js;
 
+#[js::function]
+fn cwd(ctx: js::Ctx<'_>) -> js::Result<String> {
+    std::env::current_dir()
+        .map(|p| p.to_string_lossy().to_string())
+        .map_err(|e| SystemError::from_io(e, "cwd", None).into_exception(&ctx))
+}
+
 pub fn create<'a>(ctx: &js::Ctx<'a>) -> js::Result<js::Object<'a>> {
     let process = js::Object::new(ctx.clone())?;
-    let cwd_fn = js::Function::new(ctx.clone(), |ctx: js::Ctx<'_>| -> js::Result<String> {
-        std::env::current_dir()
-            .map(|p| p.to_string_lossy().to_string())
-            .map_err(|e| SystemError::from_io(e, "cwd", None).into_exception(&ctx))
-    })?;
-    process.set("cwd", cwd_fn)?;
+    process.set("cwd", js_cwd)?;
     Ok(process)
 }
